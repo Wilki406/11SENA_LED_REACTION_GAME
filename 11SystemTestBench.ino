dfbd3f904fff0479b5aae5d70a6a0c16fee4e9ae
Benjamin <134766715+Pinky406@users.noreply.github.com>
2023-08-10 20:09:17 +1000

    Fourth Commit

--- 11SystemTestBench.ino
+++ 11SystemTestBench.ino
@@ -1,65 +1,54 @@
+#include <ezButton.h>
 #include <FastLED.h>
 
-// constants won't change
-#define NUM_LEDS 20 // Number of LED's on Strip
-#define LED_PIN 2 // Arduino pin connected to LED Strip pin
-#define STARTBUT_PIN 3 // Arduino pin connected to button's pin
- 
- // variables will change:
-///int STARTbuttonCurrentState; // the current state of button
-///int STARTbuttonLastState; // the previous state of button
-///int STARTgamestate = LOW; // the current state of LED
-bool loopRunning = false;
-
+#define LOOP_STATE_STOPPED 0
+#define LOOP_STATE_STARTED 1
+#define LED_PIN 3 // Arduino pin connected to LED Strip pin
+#define NUM_LEDS 20
 CRGB leds[NUM_LEDS];
 
+ezButton button(2);  // create ezButton object that attach to pin 2;
+int loopState = LOOP_STATE_STOPPED;
+
+
 void setup() {
+  /*******************
+   *  your setup code
+   *******************/
   Serial.begin(9600); // initialize serial
   FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
   FastLED.setMaxPowerInVoltsAndMilliamps(5,50);
-  pinMode(STARTBUT_PIN, INPUT); // set arduino pin to input pull-up mode
 
-  ///STARTbuttonCurrentState = digitalRead(STARTBUT_PIN);
-  
+  button.setDebounceTime(50); // set debounce time to 50 milliseconds
 }
 
 void loop() {
-  ///STARTbuttonLastState = STARTbuttonCurrentState; // save the last state
-  ///STARTbuttonCurrentState = digitalRead(STARTBUT_PIN); // read new state
+  button.loop(); // MUST call the loop() function first
 
-  buttonState = digitalRead(STARTBUT_PIN);
-  if((buttonState==LOW) && (prevbuttonState==HIGH))
-  {
-    loopRunning = !loopRunning
-  }
-  prevbuttonState=buttonState
-
-  if (loopRunning)
-  {
-    Serial.println("The button is pressed");
-    for(int i = 0; i < NUM_LEDS; i++) 
-    {
-    leds[i] = CRGB::Red;
-    FastLED.show();
-    delay(10);
-    leds[i] = CRGB::Black;
-      }
-    for(int i = NUM_LEDS - 1; i >= 0; i--) 
-    {
-    leds[i] = CRGB::Red;
-    FastLED.show();
-    delay(10);      
-    leds[i] = CRGB::Black;
-    }
+  if (button.isPressed()) {
+    if (loopState == LOOP_STATE_STOPPED)
+      loopState = LOOP_STATE_STARTED;
+    else // if(loopState == LOOP_STATE_STARTED)
+      loopState = LOOP_STATE_STOPPED;
   }
 
-  
-
-
+  if (loopState == LOOP_STATE_STARTED) {
+    /******************
+     *  your loop code
+     ******************/
+     for(int i = 0; i < NUM_LEDS; i++) 
+          {
+          leds[i] = CRGB::Red;
+          FastLED.show();
+          delay(20);
+          leds[i] = CRGB::Black;
+            }
+          for(int i = NUM_LEDS - 1; i >= 0; i--) 
+          {
+          leds[i] = CRGB::Red;
+          FastLED.show();
+          delay(20);      
+          leds[i] = CRGB::Black;
+          }
   }
-
-
-
-
-
-
+}
